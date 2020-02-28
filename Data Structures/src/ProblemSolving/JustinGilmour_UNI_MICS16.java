4613e43f582daa8bfc09702d28dee6d7a07ba849
Justin Gilmour <justin.gilmour@my.simpson.edu>
2020-02-27 20:56:23 -0600

    last minute update

---
+++ Data Structures/src/ProblemSolving/JustinGilmour_UNI_MICS16.java
@@ -0,0 +1,77 @@
+package ProblemSolving;
+
+import java.util.Scanner;
+
+public class JustinGilmour_UNI_MICS16 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int cases = input.nextInt();
+
+        for (int i = 0; i < cases; i++) {
+            int scalingFactor = input.nextInt();
+            System.out.println("Case: " + (i+1));
+            for (int j = 0; j < scalingFactor * 5; j++) {
+                if (j < scalingFactor || j >= scalingFactor * 4) {
+                    for (int k = 0; k < scalingFactor * 5; k++) {
+                        System.out.print("C");
+                    }
+                }else {
+                    for (int k = 0; k < scalingFactor; k++){
+                        System.out.print("C");
+                    } for (int k = 0; k < scalingFactor * 4; k++){
+                        System.out.print(" ");
+                    }
+                }
+                for (int k = 0; k < scalingFactor * 5; k++) {
+                    System.out.print(" ");
+                }
+                if (j < scalingFactor || j >= scalingFactor * 4 || (j >= scalingFactor*2) && j < scalingFactor*3) {
+                    for (int k = 0; k < scalingFactor * 5; k++) {
+                        System.out.print("S");
+                    }
+                } else if (j >= scalingFactor && j < scalingFactor*2){
+                    for(int k = 0; k < scalingFactor; k++) {
+                        System.out.print("S");
+                    }
+                    for(int k = 0; k < scalingFactor*4; k++) {
+                        System.out.print(" ");
+                    }
+                }
+                else {
+                    for(int k = 0; k < scalingFactor*4; k++) {
+                        System.out.print(" ");
+                    }
+                    for(int k = 0; k < scalingFactor; k++) {
+                        System.out.print("S");
+                    }
+                }
+                for (int k = 0; k < scalingFactor * 5; k++) {
+                    System.out.print(" ");
+                } if (j < scalingFactor || j >= scalingFactor * 4 || (j >= scalingFactor*2) && j < scalingFactor*3) {
+                    for (int k = 0; k < scalingFactor * 5; k++) {
+                        System.out.print("S");
+                    }
+                }
+                else if (j >= scalingFactor && j < scalingFactor*2){
+                    for(int k = 0; k < scalingFactor; k++) {
+                        System.out.print("S");
+                    }
+                    for(int k = 0; k < scalingFactor*4; k++) {
+                        System.out.print(" ");
+                    }
+                }
+                else {
+                    for(int k = 0; k < scalingFactor*4; k++) {
+                        System.out.print(" ");
+                    }
+                    for(int k = 0; k < scalingFactor; k++) {
+                        System.out.print("S");
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
+
+}
